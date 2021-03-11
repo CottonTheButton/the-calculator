b701781e4f817e14d500853791237a1dc00ad5d9
Cotton <elhison08@gmail.com>
2021-03-11 19:58:07 +0800

    Added folders for different topics/subjects

---
+++ Physics/ProjectileMotion.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(){
+
+    std::cout << "Remember that thex and y axes are independent from each other";
+
+    return 0;
+}
--- arithmetic.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main(){
-
-    double x{1.1};
-    std::cout << x;
-
-    return 0;
-}
