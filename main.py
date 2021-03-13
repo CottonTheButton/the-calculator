519790d52650328a2a955dea2494db7e311bcee6
Cotton <elhison08@gmail.com>
2021-03-13 19:00:20 +0800

    Added an options menu Other improvements

--- main.py
+++ main.py
@@ -2,25 +2,45 @@ import tkinter as tk
 
 root = tk.Tk()
 root.title("The Calculator")
-root.geometry("200x150")
+root.geometry("500x500")
 
-input1 = tk.Entry(root)
-input2 = tk.Entry(root)
+#-----------------------------Option Box Thing-----------------------------
+modes = ["Arithmetic", "Physics", "Calculus"]
+
+default_mode = tk.StringVar()
+default_mode.set(modes[0])
+
+calc_mode = tk.OptionMenu(root, default_mode, *modes).pack()
+
+#-----------------------------Input Fields-----------------------------
+default_input1 = tk.IntVar()
+default_input2 = tk.IntVar()
+
+default_input1.set(0)
+default_input2.set(0)
+
+input1 = tk.Entry(root, text=default_input1)
+input2 = tk.Entry(root,  text=default_input2)
 
 input1.pack()
 input2.pack()
 
-def butt_add():
+#-----------------------------Button-----------------------------
+def butt_solve():
     num1 = input1.get()
     num2 = input2.get()
     total = int(num1) + int(num2)
-    lab.config(text=f"answer = {total}")
-    
+    answer_label.config(text=f"Answer: {total}")
+    test_label.config(text=default_mode.get())
 
-butt = tk.Button(root, text="Calculate", command=butt_add)
+butt = tk.Button(root, text="Calculate", command=butt_solve)
 butt.pack()
+#-----------------------------Label-----------------------------
+answer_label = tk.Label(text="Answer: ")
+answer_label.pack()
 
-lab = tk.Label(text="")
-lab.pack()
+test_label = tk.Label(text=default_mode.get())
+test_label.pack()
 
+# root.resizable(False, False)
 root.mainloop()
